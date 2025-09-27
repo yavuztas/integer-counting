8854b168fce39dd9b4c72b18060982549f7a378a
Yavuz Tas <yavuz.tas@ing.com>
2025-09-27 22:36:49 +0200

    add hyperfine bash script to measure timing

---
+++ evaluate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+JAVA_VERSION="21.0.6-graal"
+INPUT_ARGS="./data/1M_random_numbers.txt"
+
+# Handle positional arguments
+shift $((OPTIND - 1))
+param1=$1
+param2=$2
+
+if [ "$param1" = "" ]; then
+  echo "Usage: $0 <classname>"
+  exit 1
+fi
+
+echo "using $JAVA_VERSION"
+
+# set java version
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java $JAVA_VERSION
+
+# java compile
+"$HOME"/.sdkman/candidates/java/$JAVA_VERSION/bin/javac --release "$(echo $JAVA_VERSION | cut -d. -f1)" --enable-preview -d ./bin ./src/"$param1".java
+
+if [ "$param2" == "--native" ]; then
+    NATIVE_IMAGE_OPTS="--initialize-at-build-time=$param1 -O3 -march=native --gc=epsilon -R:MaxHeapSize=64m -H:-GenLoopSafepoints --enable-preview"
+    native-image $NATIVE_IMAGE_OPTS -cp ./bin "$param1"
+fi
+
+TIMEOUT="gtimeout -v 30" # in seconds, from `brew install coreutils`
+HYPERFINE_OPTS="--warmup 10 --runs 10 --output ./$param1.out" # --show-output
+
+if [ "$param2" == "--native" ]; then
+    imageName=$(echo "$param1" | tr '[:upper:]' '[:lower:]')
+    echo "Picking up native image './$imageName'" 1>&2
+    hyperfine $HYPERFINE_OPTS "$TIMEOUT ./$imageName $INPUT_ARGS"
+else
+    JAVA_OPTS="-Xmx64m -XX:MaxGCPauseMillis=1 -XX:-AlwaysPreTouch -XX:+UseSerialGC -XX:+TieredCompilation --enable-preview"
+    echo "Choosing to run the app in JVM mode" 1>&2
+    hyperfine $HYPERFINE_OPTS "$TIMEOUT sh -c '$HOME/.sdkman/candidates/java/$JAVA_VERSION/bin/java $JAVA_OPTS -classpath ./bin $param1 $INPUT_ARGS'"
+fi
--- readme.md
+++ readme.md
@@ -1,3 +1,29 @@
 # Stackoverflow Challenge - Integer Counting
 This is a challenge focusing on performance and optimization. 
 Details are on the site: https://stackoverflow.com/beta/challenges/79766578/code-challenge-6-integer-counting
+
+## How to run?
+Install JDK using sdk man:
+```
+sdk install java 25-graal
+```
+Install hyperfine and coreutils:
+```
+brew install hyperfine
+brew install coreutils # for gtimeout
+```
+To run different solutions use classname as parameter in `evaluate.sh <classname>`:
+```
+./evaluate.sh Main
+```
+## Solutions
+
+### Using MappedByteArray
+Run `evaluate.sh Solution1`.
+
+### Using Java Memory Api (preview in Java 21)
+Run `evaluate.sh Solution2`.
+
+### Using UNSAFE
+Run `evaluate.sh MainUnsafe`. This solution is just to push the limits for fun. It is also fully compatible and works in Graalvm Native.
+For native run `evaluate.sh MainUnsafe --native`
